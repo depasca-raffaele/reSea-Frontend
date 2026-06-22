f38cc33ffe309610a7e5f75750294247c1e3d481
Francesco Cassese <francescocassese9@gmail.com>
2026-06-22 11:56:40 +0200

    fix: messaggio in caso di prodotto inesistente

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import useFetch from "../hooks/useFetch";
+import { Navigate, useParams } from "react-router-dom";
 
 function ProductDetail() {
-    return(
+
+    const { slug } = useParams();
+    const { data, loading, error } = useFetch(`/products/${slug}`)
+
+    if (loading) return <p>Caricamento in corso...</p>
+    if (error) {
+        return (
+            <div>
+                <h1>Siamo spiacenti!</h1>
+                <p>Non abbiamo trovato alcun prodotto associato al nome: <strong>{slug}</strong></p>
+            </div>
+        );
+    }
+    return (
         <><h1>dettagli prodotti</h1></>
     )
 }
